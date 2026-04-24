22ec833ca7336a771b27735840c78a4403c7b225
Arjun <arjunsemwal02@gmail.com>
2026-04-24 17:44:00 +0530

    search algos added.

--- src/dsa/patterns/Sqrpattern.java
+++ src/dsa/patterns/Sqrpattern.java
@@ -2,12 +2,53 @@ package dsa.patterns;
 
 public class Sqrpattern {
     public static void main(String[] args) {
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                System.out.print(4);
+        pattern1(5);
+        System.out.println();
+
+        starPattern(6);
+        System.out.println();
+
+        numberPattern(6);
+        System.out.println();
+
+        horizontalNumbers(8);
+    }
+
+    public static void pattern1(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(n);
             }
             System.out.println();
         }
-
     }
+
+    public static void starPattern(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void numberPattern(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(j + 1);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void horizontalNumbers(int n) {
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                System.out.print(i + 1);
+            }
+            System.out.println();
+        }
+    }
+
+
 }
