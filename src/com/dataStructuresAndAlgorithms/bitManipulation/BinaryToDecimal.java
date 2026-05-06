62c826d7805540f5b540a29971205998d4768a82
Arjun <arjunsemwal02@gmail.com>
2026-05-06 23:59:24 +0530

    Added binary to dec and vice versa to bit manipulation package.

---
+++ src/com/dataStructuresAndAlgorithms/bitManipulation/BinaryToDecimal.java
@@ -0,0 +1,25 @@
+package com.dataStructuresAndAlgorithms.bitManipulation;
+
+public class BinaryToDecimal {
+
+    public static void main(String[] args) {
+//        System.out.println(binaryToDecimal("1010"));
+        System.out.println(binaryToDecimal("11011001"));
+    }
+
+    private static int binaryToDecimal(String bin){
+
+        int res = 0;
+        int powOf2 = 1;
+
+        res += (bin.charAt(bin.length() - 1) - '0') * 1;
+//        res += (bin.charAt(bin.length() - 1) - '0') * 2;
+
+        for(int i = bin.length() - 2; i >= 0; i--){
+            powOf2 *= 2;
+            res += (bin.charAt(i) - '0') * powOf2;
+        }
+
+        return res;
+    }
+}
--- src/com/dataStructuresAndAlgorithms/bitManipulation/DecimalToBinary.java
+++ src/com/dataStructuresAndAlgorithms/bitManipulation/DecimalToBinary.java
@@ -1,4 +1,43 @@
 package com.dataStructuresAndAlgorithms.bitManipulation;
 
 public class DecimalToBinary {
+
+    public static void main(String[] args) {
+        System.out.println(decimalToBinary(217));
+//        System.out.println(decToBinRec(217));
+//        System.out.println(printString());
+    }
+
+    public static String decimalToBinary(int n){
+
+        String s = "";
+
+        while(n >= 1){
+            s = (n % 2) + s;
+            n /= 2;
+        }
+
+        return s;
+    }
+
+    private static String decToBinRec(int n){
+
+        if(n == 1) return "1";
+
+        return decToBinRec(n / 2) + n % 2;
+
+    }
+
+    public static String printString(){
+
+        String s = "";
+
+        s = s + "a";
+        s = s + 3;
+        s = s + 4;
+        s = s + "b";
+
+        return s;
+    }
+
 }
